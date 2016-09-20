b27c664585ff3af1de4e181f337a79a6b94b09c9
Konrad Wyrozumski <konrad.wyrozumski@gmail.com>
2016-09-20 19:57:22 +0200

    Added installation of typings to deployment

--- deploy.cmd
+++ deploy.cmd
@@ -102,6 +102,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install
   call :ExecuteCmd !NPM_CMD! install jspm -g
+  call :ExecuteCmd !NPM_CMD! install typings -g
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
